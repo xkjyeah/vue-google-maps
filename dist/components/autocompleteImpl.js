10a9127cdfff77afa3f330fe7d2cfdbe945f136e
Ronald Lima <ronaldlima@kofre.com.br>
2016-12-22 18:25:59 -0300

    * Be compatible with autocomplete

--- dist/components/autocompleteImpl.js
+++ dist/components/autocompleteImpl.js
@@ -20,6 +20,10 @@ var _simulateArrowDown = require('../utils/simulateArrowDown.js');
 
 var _simulateArrowDown2 = _interopRequireDefault(_simulateArrowDown);
 
+var _mapElementMixin = require('./mapElementMixin');
+
+var _mapElementMixin2 = _interopRequireDefault(_mapElementMixin);
+
 var _getPropsValuesMixin = require('../utils/getPropsValuesMixin.js');
 
 var _getPropsValuesMixin2 = _interopRequireDefault(_getPropsValuesMixin);
@@ -32,13 +36,21 @@ var _assert2 = _interopRequireDefault(_assert);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
+var placeInputProps = {
+  bounds: {
+    type: Object,
+    twoWay: true
+  },
+  componentRestrictions: {
+    type: Object
+  },
+  types: {
+    type: Array
+  }
+};
 var props = {
   bounds: {
     type: Object
-  },
-  defaultPlace: {
-    type: String,
-    default: ''
   },
   componentRestrictions: {
     type: Object,
@@ -51,40 +63,99 @@ var props = {
     }
   },
   placeholder: {
-    required: false,
     type: String
   },
   selectFirstOnEnter: {
-    require: false,
+    type: Boolean,
+    default: false
+  },
+  autoFitOnUpdatePlace: {
+    type: Boolean,
+    default: false
+  },
+  mapEmbedded: {
     type: Boolean,
     default: false
   }
 };
 
 exports.default = {
-  mixins: [_getPropsValuesMixin2.default],
+  mixins: [_mapElementMixin2.default, _getPropsValuesMixin2.default],
+  props: props,
+  data: function data() {
+    return {
+      placeInputObj: {
+        place: undefined
+      }
+    };
+  },
 
+  computed: {
+    local_bounds: {
+      get: function get() {
+        return this.bounds;
+      },
+      set: function set(value) {
+        this.$emit('bounds-changed', value);
+      }
+    },
+    local_componentRestrictions: function local_componentRestrictions() {
+      return this.componentRestrictions;
+    },
+    local_types: function local_types() {
+      return this.types;
+    },
+    local_placeholder: function local_placeholder() {
+      return this.placeholder;
+    },
+    local_selectFirstOnEnter: function local_selectFirstOnEnter() {
+      return this.selectFirstOnEnter;
+    },
+    local_autoFitOnUpdatePlace: function local_autoFitOnUpdatePlace() {
+      return this.autoFitOnUpdatePlace;
+    },
+    local_mapEmbedded: function local_mapEmbedded() {
+      return this.mapEmbedded;
+    }
+  },
+  created: function created() {
+    this.$hybridComponent = !this.local_mapEmbedded;
+    this.autocompleter = null;
+  },
   mounted: function mounted() {
     var _this = this;
 
     var input = this.$refs.input;
-
     _manager.loaded.then(function () {
+      window.i = input;
       var options = _lodash2.default.clone(_this.getPropsValues());
-      if (_this.selectFirstOnEnter) {
+      if (_this.local_selectFirstOnEnter) {
         (0, _simulateArrowDown2.default)(_this.$refs.input);
       }
 
       (0, _assert2.default)(typeof google.maps.places.Autocomplete === 'function', "google.maps.places.Autocomplete is undefined. Did you add 'places' to libraries when loading Google Maps?");
 
-      _this.$autocomplete = new google.maps.places.Autocomplete(_this.$refs.input, options);
-      (0, _propsBinder2.default)(_this, _this.autoCompleter, _lodash2.default.omit(props, ['placeholder', 'place', 'selectFirstOnEnter', 'defaultPlace']));
-
-      _this.$autocomplete.addListener('place_changed', function () {
-        _this.$emit('place_changed', _this.$autocomplete.getPlace());
-      });
+      _this.autocompleter = new google.maps.places.Autocomplete(_this.$refs.input, options);
+      (0, _propsBinder2.default)(_this, _this.autocompleter, placeInputProps);
+      _this.autocompleter.addListener('place_changed', _this.placeChanged);
     });
   },
+  deferredReady: function deferredReady() {
+    if (this.local_mapEmbedded) {
+      if (this.$map) {
+        this.$map.controls[google.maps.ControlPosition.TOP_LEFT].push(this.$refs.input);
+      }
+    }
+  },
 
-  props: props
+  methods: {
+    autoFitPlace: function autoFitPlace(place) {
+      if (typeof this.$map === 'undefined' || typeof place.geometry === 'undefined') return;
+      this.$map.fitBounds(place.geometry.viewport);
+    },
+    placeChanged: function placeChanged() {
+      this.$emit('place_changed', this.autocompleter.getPlace());
+      if (this.local_autoFitOnUpdatePlace) this.autoFitPlace(this.autocompleter.getPlace());
+    }
+  }
 };
--- src/components/autocompleteImpl.js
+++ src/components/autocompleteImpl.js
@@ -2,19 +2,26 @@ import _ from 'lodash'
 import eventBinder from '../utils/eventsBinder.js'
 import propsBinder from '../utils/propsBinder.js'
 import downArrowSimulator from '../utils/simulateArrowDown.js'
+import MapElementMixin from './mapElementMixin';
 import getPropsValuesMixin from '../utils/getPropsValuesMixin.js'
-import {
-  loaded
-} from '../manager.js'
+import {loaded} from '../manager.js'
 import assert from 'assert';
 
-const props = {
+const placeInputProps = {
   bounds: {
     type: Object,
+    twoWay: true
   },
-  defaultPlace: {
-    type: String,
-    default: '',
+  componentRestrictions: {
+    type: Object
+  },
+  types: {
+    type: Array
+  }
+}
+const props = {
+  bounds: {
+    type: Object
   },
   componentRestrictions: {
     type: Object,
@@ -22,43 +29,103 @@ const props = {
   },
   types: {
     type: Array,
-    default: function() {
+    default() {
       return [];
     }
   },
   placeholder: {
-    required: false,
     type: String
   },
   selectFirstOnEnter: {
-    require: false,
+    type: Boolean,
+    default: false
+  },
+  autoFitOnUpdatePlace: {
+    type: Boolean,
+    default: false
+  },
+  mapEmbedded: {
     type: Boolean,
     default: false
   }
 }
 
 export default {
-  mixins: [getPropsValuesMixin],
-
-  mounted() {
+  mixins: [MapElementMixin, getPropsValuesMixin],
+  props: props,
+  data(){
+    return {
+      placeInputObj:{
+        place: undefined
+      }
+    }
+  },
+  computed: {
+    local_bounds: {
+      get(){
+        return this.bounds;
+      },
+      set(value){
+        this.$emit('bounds-changed', value);
+      }
+    },
+    local_componentRestrictions(){
+      return this.componentRestrictions;
+    },
+    local_types(){
+      return this.types;
+    },
+    local_placeholder(){
+      return this.placeholder;
+    },
+    local_selectFirstOnEnter(){
+      return this.selectFirstOnEnter;
+    },
+    local_autoFitOnUpdatePlace(){
+      return this.autoFitOnUpdatePlace;
+    },
+    local_mapEmbedded(){
+      return this.mapEmbedded;
+    }
+  },
+  created(){
+    this.$hybridComponent = !this.local_mapEmbedded;
+    this.autocompleter = null;
+  },
+  mounted () {
     const input = this.$refs.input;
-
     loaded.then(() => {
+      window.i = input;
       const options = _.clone(this.getPropsValues());
-      if (this.selectFirstOnEnter) {
+      if (this.local_selectFirstOnEnter) {
         downArrowSimulator(this.$refs.input);
       }
 
       assert(typeof(google.maps.places.Autocomplete) === 'function',
         "google.maps.places.Autocomplete is undefined. Did you add 'places' to libraries when loading Google Maps?")
 
-      this.$autocomplete = new google.maps.places.Autocomplete(this.$refs.input, options);
-      propsBinder(this, this.autoCompleter, _.omit(props, ['placeholder', 'place', 'selectFirstOnEnter', 'defaultPlace']));
-
-      this.$autocomplete.addListener('place_changed', () => {
-        this.$emit('place_changed', this.$autocomplete.getPlace())
-      })
+      this.autocompleter = new google.maps.places.Autocomplete(this.$refs.input, options);
+      propsBinder(this, this.autocompleter, placeInputProps);
+      this.autocompleter.addListener('place_changed', this.placeChanged);
     })
   },
-  props: props,
+  deferredReady() {
+    if (this.local_mapEmbedded) {
+      if (this.$map) {
+        this.$map.controls[google.maps.ControlPosition.TOP_LEFT].push(this.$refs.input);
+      }
+    }
+  },
+  methods: {
+    autoFitPlace(place){
+      if (typeof this.$map === 'undefined' || typeof place.geometry === 'undefined')
+        return;
+      this.$map.fitBounds(place.geometry.viewport);
+    },
+    placeChanged() {
+      this.$emit('place_changed', this.autocompleter.getPlace());
+      if (this.local_autoFitOnUpdatePlace)
+        this.autoFitPlace(this.autocompleter.getPlace());
+    }
+  }
 }
